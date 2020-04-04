a20b5c7b58de13d8286d561a875fcf59368ab48a
Darvi1811 <61952350+Darvi1811@users.noreply.github.com>
2020-04-04 17:45:36 +0300

    Add files via upload

---
+++ FreshBot.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+
+#токен
+token = "Njk1MjI3MDk5OTA4Mjc2MjI0.XoXGxA.Sam3phZ3OBfzyl4enbnUxevUmJw"
+
+#префикс 
+bot = commands.Bot(command_prefix = "*")
+
+@bot.event
+
+#Подключение
+async def on_ready():
+	print("[SUCCES] Бот в сети!")
+
+
+@bot.command()
+async def embed(ctx):
+
+	
+	
+	embed = discord.Embed(
+			colour=discord.Colour.red(),
+			title="**Counter Strike: Global Offensive**",
+			description='**Всем привет! Добавлена категория "Counter Strike: Global Offensive", там можно поиграть в ММ или Напарники.\nЕще добавлены новости по теме CS GO, если вы в <#695659524773642301> получите роль "CS GO News".\nТо будете получать уведомления при начале  трансляции csgomc_ru!**'
+	)
+
+		
+	await ctx.send(embed=embed)
+	print("[SUCCES] Новости отправлены!")
+	
+
+@bot.command()
+
+async def hello(ctx):
+	
+	await ctx.send("**╔┓┏╦━━╦┓╔┓╔━━╗ @everyone\n║┗┛║┗━╣┃║┃║╯╰║ @everyone\n║┏┓║┏━╣┗╣┗╣╰╯║ @everyone\n╚┛┗╩━━╩━╩━╩━━╝ @everyone**")
+	print("[SUCCES] Hello отправлено!")
+
+
+
+
+
+bot.run(token)
